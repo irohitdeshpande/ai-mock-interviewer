8e84a2d50e7470abe29783d7784446275cd56eae
irohitdeshpande <rohit.sd@somaiya.edu>
2025-03-21 12:06:28 +0530

    added header component, for both web and mobile view

--- src/components/toggle-container.tsx
+++ src/components/toggle-container.tsx
@@ -1,5 +1,38 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { useAuth } from "@clerk/clerk-react"
+import { NavigationRoutes } from "./navigation-routes"
+import { NavLink } from "react-router-dom"
+import { cn } from "@/lib/utils"
+import { Menu } from "lucide-react"
+
 export const ToggleContainer = () => {
+  const { userId } = useAuth()
   return (
-    <div>ToggleContainer</div>
+    <Sheet>
+      <SheetTrigger className="block">
+        <Menu className = "w-6 h-6 md:hidden" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle />
+        </SheetHeader>
+        <nav className = "items-start flex flex-col gap-5">
+        <NavigationRoutes isMobile />
+        {userId && (
+          <NavLink to='/generate' className={({ isActive }) => cn("text-base text-neutral-600", isActive && "text-neutral-900 font-semibold"
+          )}>
+            Take an Interview
+          </NavLink>
+        )}
+        </nav>
+      </SheetContent>
+    </Sheet>
+
   )
 }
